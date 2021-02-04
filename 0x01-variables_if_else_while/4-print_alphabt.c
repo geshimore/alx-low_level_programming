5c2d29c91898806020bd4bffab324ce95c4f1773
root <peter.ngugi@mail.com>
2021-02-04 18:16:09 +0000

    alphabets with e and q exception

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
