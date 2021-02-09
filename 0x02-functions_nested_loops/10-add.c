23bd755e4b97483703a5e941896369532f887ee8
root <peter.ngugi@mail.com>
2021-02-09 10:20:57 +0000

    adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - a function that adds two integers
+ * @a: input
+ * @b: input
+ * Return: sum of the integers
+*/
+
+int add(int a, int b)
+{
+	int res = a + b;
+
+	return (res);
+}
