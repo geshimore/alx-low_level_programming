cc36d78341c08682a100071c259ce22bd3d4b059
root <peter.ngugi@mail.com>
2021-02-09 10:11:30 +0000

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+
+/**
+ * times_table - a function to print a 9 times table
+ * r = row, c = column, d = digit
+ * Return: 9 times table
+*/
+
+void times_table(void)
+{
+	int r, c, d;
+
+	for (r = 0; r <= 9; r++)
+	{
+		_putchar('0');
+
+		_putchar(',');
+
+		_putchar(' ');
+
+		for (c = 1; c <= 9; c++)
+		{
+			d = (r * c);
+
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+
+			if (c < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+
+
+}
