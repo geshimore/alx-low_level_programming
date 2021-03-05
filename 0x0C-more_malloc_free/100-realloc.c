13ca5646b873031c893701dbd8059d7ac809429b
geshimore <peter.ngugi@mail.com>
2021-03-05 20:31:14 +0000

    c-more malloc, free

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - a function that reallocates a memory block using malloc and free
+ * @ptr: pointer to previous memory block
+ * @old_size: size of allocated space for `ptr`
+ * @new_size: size of new allocated space
+ * Return: Pointer to new memory block, or NULL if error
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+
+{
+	char *p, *copy;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr != NULL && new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+		return (p);
+	}
+
+	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
+	copy = ptr;
+	for (i = 0; i < old_size; i++)
+		p[i] = copy[i];
+	free(ptr);
+	return (p);
+}
