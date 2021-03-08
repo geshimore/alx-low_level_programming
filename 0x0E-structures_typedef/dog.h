50b04c6f1a14be41c5dc6b3cde26bc4f91fd7b3d
geshimore <peter.ngugi@mail.com>
2021-03-08 22:48:02 +0000

    structures and typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a program to create struct
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: the owner of the dog
+ *
+ * Description: creating a struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
