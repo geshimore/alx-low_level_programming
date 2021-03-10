e004d10b0aa7599dce93e12f16086ddd6d74670d
geshimore <peter.ngugi@mail.com>
2021-03-10 20:16:46 +0000

    function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,11 +11,15 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 
 {
+	unsigned int i;
 
-	size_t i;
+	if (action != NULL && array != NULL)
 
-	for (i = 0; i < size; i++)
+	{
 
-		action(array[i]);
+		for (i = 0; i < size; i++)
 
+			action(array[i]);
+
+	}
 }
