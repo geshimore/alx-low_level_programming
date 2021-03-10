b8e6844c3a41334d8e395ce28c6e6e7f477d87ba
geshimore <peter.ngugi@mail.com>
2021-03-10 20:09:44 +0000

    function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+* print_name - Print a name from a function pointer
+* @name: char string
+* @f: function pointer that takes a string argument
+* Return: always 0
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+
+	f(name);
+
+}
