f85a3f8b141f044e29d3d4b280c98bbb52623e4c
geshimore <peter.ngugi@mail.com>
2021-03-18 18:38:12 +0000

    singly linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * *add_node - function that adds a new node at the beginning of a list_t list
+ * @head: Address of first element of the list
+ *@str: pointer to string we want to print
+ * Return: the address of the new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+
+	struct list_s *temp = NULL;
+
+	int i;
+
+	temp = (struct list_s *)malloc(sizeof(struct list_s));
+
+	if (temp == NULL)
+
+		return (NULL);
+
+	temp->str = strdup(str);
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	temp->len = i;
+
+	temp->next = *head;
+
+	*head = temp;
+
+	return (temp);
+
+}
