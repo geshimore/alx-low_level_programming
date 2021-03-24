cb089f0a5d2c3b112e8f0ac9e92f6d45fad3e86d
root <peter.ngugi@mail.com>
2021-03-24 18:48:32 +0000

    more singly linked lists

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint_safe - a function that prints a listint_t linked list.
+ * @head: first element of the list
+ * Return: the number of nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+
+	const listint_t *temp = NULL;
+	const listint_t *temp2 = NULL;
+	int i, count;
+
+	i = 0;
+	temp = head;
+	while (temp)
+	{
+		printf("[%p] %d\n", (void *)temp, temp->n);
+		i++;
+		temp = temp->next;
+		temp2 = head;
+		count = 0;
+		while (count < i)
+		{
+			if (temp == temp2)
+			{
+				printf("-> [%p] %d\n", (void *)temp, temp->n);
+				return (i);
+			}
+			temp2 = temp2->next;
+			count++;
+		}
+		if (head == NULL)
+			exit(98);
+	}
+	return (i);
+}
---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint_safe - a function that frees a listint_t list.
+ * @h: first element of the list
+ * Return: the size of the list that was dddd
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+
+	listint_t *temp;
+	listint_t *aux;
+
+	size_t i;
+
+	i = 0;
+
+	temp = *h;
+	if (h == NULL)
+		return (0);
+	while (temp != NULL)
+	{
+		i++;
+
+		aux = temp;
+		temp = temp->next;
+
+		free(aux);
+
+		if (aux < temp)
+		{
+			break;
+		}
+		*h = NULL;
+	}
+	return (i);
+}
